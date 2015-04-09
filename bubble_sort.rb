7906f80e19a41837fdc246a107d737816d3e43d3
Josh Cass <joshuacass@gmail.com>
2015-04-09 10:01:29 -0600

    Master branch that works

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -36,55 +36,7 @@ end
 
 
 
-
-# class BubbleSort
-#   attr_reader :array, :index, :swapped
-#
-#   def initialize(array)
-#     @array = array
-#     @index = 0
-#     @swapped = 0
-#   end
-#
-#   def diff?
-#     array[index] > array[index + 1]
-#   end
-#
-#   def swapped?
-#     swapped >= 1
-#   end
-#
-#   def swap
-#     array[index], array[index + 1] = array[index + 1], array[index]
-#     @swapped += 1
-#     array
-#   end
-#
-#   def increase_index
-#     if index < array.length - 1
-#       @index += 1
-#     end
-#   end
-#
-#   def sorter_engine
-#     while @swapped < 1
-#       if diff?
-#         swap
-#         increase_index
-#       elsif  swapped?
-#           @swapped = 0
-#           @index = 0
-#       else
-#         increase_index
-#       end
-#     end
-#     array
-#   end
-# end
-
-
-
-
+# NON OOP WAY
 
 # sequence = [5, 4, 3, 2, 1, 0]
 #
